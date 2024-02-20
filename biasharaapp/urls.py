1551673e92a4737298d8850f4ebb7cd7ab0a319d
ketuyio <ketuyioelias630@gmail.com>
2024-02-20 16:31:36 +0300

    initial commit

---
+++ biasharaapp/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from biasharaapp import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home),
+    path('gallery/', views.gallery),
+    path('about/', views.about),
+    path('contact/', views.contact),
+    path('collection/', views.collection),
+]
---
+++ biasharaapp/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+# Create your views here.
+def home(request):
+    return render(request,"index.html")
+
+def gallery(request):
+    return render(request,"gallery.html")
+
+def about(request):
+    return render(request,"about.html")
+
+def contact(request):
+    return render(request,"contact.html")
+
+def collection(request):
+    return render(request,"collection.html")
